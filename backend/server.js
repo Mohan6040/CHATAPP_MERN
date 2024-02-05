0c35fa3d070f2cfb5da5cc23a270979e073208b8
mohankumar <ntrmohan837@gmail.com>
2024-02-05 13:48:35 +0530

    4th part completed - socket

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
+import path from "path";
 import express from "express";
-// const express=require('express');
 import dotenv from "dotenv";
-// const dotenv=require("dotenv");
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -9,9 +8,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,24 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   //root route http://localhost:5000/
-//   res.send("Hello World!!!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("/api/auth/signup",(req,res)=>{
-//     console.log("signup route");
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-// app.get("/api/auth/login",(req,res)=>{
-//     console.log("login route");
-// });
-
-// app.get("/api/auth/logout",(req,res)=>{
-//     console.log("logout route");
-// });
-
-app.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server Running on port ${PORT}`);
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
